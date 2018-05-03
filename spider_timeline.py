e56b05b4867628ee88089eac4e7028b66788904f
sunhailin-Leo <379978424@qq.com>
2018-05-04 02:05:07 +0800

    add some chart options

--- spider_timeline.py
+++ spider_timeline.py
@@ -38,33 +38,40 @@ def draw_line(attr_list, value_list, rank_list):
                 subtitle=
                 "Made By sunhailin-Leo (数据从21点23分开始采集) --- "
                 "截止到:{}".format(time.strftime("%Y年%m月%d日 %H时%M分%S秒")),
+                title_pos="center",
                 subtitle_text_size=14,
                 subtitle_color="#A8A8A8")
     line.add("微博热搜量", attr_list, value_list,
-             legend_pos="center",
-             legend_top="bottom",
-             mark_point=["min", "average"],
+             is_smooth=True,
+             is_datazoom_show=True,
+             datazoom_range=[0, 75],
+             legend_pos="80%",
+             legend_top="top",
+             mark_point=["min", "max", "average"],
              mark_line=['min', "max", "average"],
              mark_point_symbolsize="72",
              is_more_utils=True,
              xaxis_name="时间",
              xaxis_name_pos="end",
              yaxis_name="微博热搜量(人/次)",
-             yaxis_name_pos="start")
+             yaxis_name_pos="end")
 
     # 微博热搜排名
     line_1 = Line()
     line_1.add("微博热搜排名", attr_list, rank_list,
-               legend_pos="center",
-               legend_top="bottom",
-               mark_point=["max", "average"],
+               is_smooth=True,
+               is_datazoom_show=True,
+               datazoom_range=[0, 75],
+               mark_point=["min", "max", "average"],
                mark_point_symbolsize="72",
                is_more_utils=True,
                is_yaxis_inverse=True,
                xaxis_name="时间",
                xaxis_name_pos="end",
+               yaxis_min=5,
                yaxis_name="排名位置",
-               yaxis_name_pos="start")
+               yaxis_name_pos="start",
+               yaxis_formatter="名")
 
     # 复合图
     overlap.add(line)
